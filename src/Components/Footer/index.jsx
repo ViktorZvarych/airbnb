71ce572b09bf6916c96701a5d1b2b9e55ae495c0
Viktor Zvarych <viktor.zvarych@gmail.com>
2023-11-28 16:37:09 +0200

    add state to current year in Footer component

--- src/Components/Footer/index.jsx
+++ src/Components/Footer/index.jsx
@@ -1,9 +1,12 @@
+import { useState } from 'react'
 import styles from './footer.module.css'
 
 export default function Footer() {
+    const [currentYear, setCurrentYear] = useState((new Date()).getFullYear());
+
     return (
         <div className={styles.footer}>
-            <p><small>© 2023 Viktor Zvarych</small></p>
+            <p><small>© {currentYear} Viktor Zvarych</small></p>
             <p><small>Designed by Scrimba</small></p>
         </div>
     )
